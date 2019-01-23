2ba914b750130fb8ecd2b4070665a8924d762ba8
Tyler Miller <tyler.rw.miller@gmail.com>
2019-01-22 21:12:36 -0500

    Cleanup

--- Composite-TopicsAndLecturesStarter/src/model/Topic.java
+++ Composite-TopicsAndLecturesStarter/src/model/Topic.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Topic extends Module {
-//    private List<Lecture> lectures = new ArrayList<>();
-//    private List<Video> videos = new ArrayList<>();
-//    private List<Topic> topics = new ArrayList<>();
     private String name;
     private List<Module> modules = new ArrayList<>();
 
@@ -18,28 +15,9 @@ public class Topic extends Module {
         modules.add(m);
     }
 
-//    public void addTopic(Topic t) {
-//        topics.add(t);
-//    }
-//
-//    public void addLecture(Lecture lecture) {
-//        lectures.add(lecture);
-//    }
-//    public void addVideo (Video video) {
-//        videos.add(video);
-//    }
-
     public void display(String indentLevel) {
         System.out.println(indentLevel + name);
-//        for (Lecture lecture : lectures) {
-//            lecture.display(indentLevel + indentLevel);
-//        }
-//        for (Video video : videos) {
-//            video.display(indentLevel + indentLevel);
-//        }
-//        for (Topic topic : topics) {
-//            topic.display(indentLevel + indentLevel);
-//        }
+
         for (Module module : modules) {
             module.display(indentLevel + indentLevel);
         }
